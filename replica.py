398f133bc6cee4d2e79b8d30672e0f8f2f6978cf
spark166 <joon@joons-MacBook-Pro.local>
2018-12-14 20:08:47 -0500

    replica, publish, services

---
+++ replica.py
@@ -0,0 +1,129 @@
+"""
+Multi-threaded server.  Once a connection is accepted, a new 
+thread is created to serve the client.
+"""
+import random
+import socket
+from thread import start_new_thread
+import time
+
+PORT = 12346
+#replica_port =12346
+hostname = socket.gethostname()    
+host = socket.gethostbyname(hostname)    
+print("replica is running on: " + host)
+#host = '130.85.241.172'  #localhost
+#host = '127.0.0.1'  #localhost
+#host = socket.gethostname()  #return this machine's hostname
+
+add_wsdl = {}  #dict to hold wsdl addresses
+add_load = {}  #load dictionary
+sub_wsdl = {}
+sub_load = {}
+hel_wsdl = {}
+hel_load = {}
+
+#each new thread executes this function
+def contacted(clientsocket):
+    #receive string from client
+    wsdl_response=""
+    mssg = clientsocket.recv(1024)
+    print('mssg: ', mssg)
+
+#####send to replica
+#    #create a socket object with IPv4 and TCP protocol
+#    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    #connect to a server with host address and specified port
+#    s1.connect((replica, replica_port))
+#
+#    #send wsdl address to the registry server
+#    print ('publishing wsdl to replica')
+#    s1.send(mssg)
+#    s1.close()
+####
+
+    tok = mssg.split(" ")
+    # if message received is a wsdl, not a request
+    if (mssg[0:3] == 'Add'):
+        add_wsdl[tok[0]] = tok[1]
+        add_load[tok[0]] = 0
+        print('wsdl array has: ')
+        for key in add_wsdl:
+            print(key + ": " + add_wsdl[key])
+    elif (mssg[0:3] == 'Sub'):
+        sub_wsdl[tok[0]] = tok[1]
+        sub_load[tok[0]] = 0
+        print('wsdl array has: ')
+        for key in sub_wsdl:
+            print(key + ": " + sub_wsdl[key])
+    elif (mssg[0:3] == 'Hel'):
+        hel_wsdl[tok[0]] = tok[1]
+        hel_load[tok[0]] = 0
+        print('wsdl array has: ')
+        for key in hel_wsdl:
+            print(key + ": " + hel_wsdl[key])
+    # for request for service
+    else:
+        # find min load
+        if (tok[1][0:3]=='Add'):
+            min_key_val = min(add_load.items(), key=lambda x: x[1]) 
+            wsdl_response = add_wsdl[min_key_val[0]]
+            add_load[min_key_val[0]] += 1
+            print("wsdl_response: "+wsdl_response)
+            print('addition load: ')
+            for key, val in add_load.items():
+                print(key, val)
+        
+        elif (tok[1][0:3]=='Sub'):
+            min_key_val = min(sub_load.items(), key=lambda x: x[1]) 
+            wsdl_response = sub_wsdl[min_key_val[0]]
+            sub_load[min_key_val[0]] += 1
+            print("wsdl_response: "+wsdl_response)
+            print('subtract load: ')
+            for key, val in sub_load.items():
+                print(key, val)
+
+        elif (tok[1][0:3]=='Hel'):
+            min_key_val = min(hel_load.items(), key=lambda x: x[1]) 
+            wsdl_response = hel_wsdl[min_key_val[0]]
+            hel_load[min_key_val[0]] += 1
+            print("wsdl_response: "+wsdl_response)
+            print('hello load: ')
+            for key, val in hel_load.items():
+                print(key, val)
+        else:
+            print("UNRECOGNIZED REQUEST")
+
+#        if (len(wsdl)==1):
+#            r=0
+#        else:
+#            r = random.randint(0,len(wsdl)-1)
+#        print("length of wsdl array: ", len(wsdl), " random value selected: ", r)
+#        wsdl_response = wsdl[r]
+        
+        clientsocket.send(wsdl_response)
+
+    #delay added to test multi-threading parallelism
+    #time.sleep(1)
+    #send reverse message to the client
+    #clientsocket.send(mssg_reverse)
+    clientsocket.close()
+
+
+#create a socket object with IPv4 and TCP protocol
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#bind the socket to host addr and port
+s.bind((host, PORT))
+#listen to socket, queue as many as 8 connect requests
+s.listen(8)
+
+while True:
+    #accept connection requests, this method is blocking
+    (clientsocket, addr) = s.accept()
+    print "Accepted connection from ", addr
+    #multi-threading by start a new thread, which executes the argument's function
+    start_new_thread(contacted, (clientsocket,))
+
+s.close()
+
+
---
+++ send_wsdl-sub2.py
@@ -0,0 +1,55 @@
+tomcat_directory = '/Users/joon/Downloads/apache-tomcat-9.0.13/'
+registry_hard = '130.85.251.172'  #######################registry address
+SERVICE_NAME = 'Subtract2 '
+
+import socket  #socket library
+import sys
+"""
+A simple client, which establishes a TCP connection with a server.
+Prompt for a user input, which is sent to the server. Then, waits
+for a message from the server, which gets displayed.
+"""
+PORT = 12345
+#hostname = socket.gethostname()  #return a hostname of this machine
+
+f = open('Subtracter2/wsdl/Subtracter2.wsdl', 'r+')
+lines = [line for line in f.readlines()]
+f.close()
+
+#extract the wsdl address
+for line in lines:
+    tok = line.split('=')
+    #print(tok)
+    if (tok[0] == '         <wsdlsoap:address location'):
+        tok2 = tok[1].split('"')
+        wsdl_addr = tok2[1]
+        #print(tok2)
+        print('wsdl address extracted is: ', wsdl_addr)
+
+        hostname = socket.gethostname()    
+        server_ip = socket.gethostbyname(hostname)    
+        tomcat_addr = wsdl_addr[:7]+server_ip+wsdl_addr[16:]
+        print('wsdl address to send is: ', tomcat_addr)
+
+#create a socket object with IPv4 and TCP protocol
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connect to a server with registry address and specified port
+s.connect((registry_hard, PORT))
+
+#prompt for a user input
+#mssg = raw_input("Enter message:")
+#if len(mssg)==0:  #if the user input is empty
+#    mssg=' '
+
+#send wsdl address to the registry server
+print ('sending the extracted wsdl address to registry')
+mssg = SERVICE_NAME+tomcat_addr
+print(mssg)
+s.send(mssg)
+
+#receive and print to a screen
+#print s.recv(1024)
+
+s.close()
+
+
