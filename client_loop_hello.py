1dfcd52a1634984dddd639360ea6b31292955467
spark166 <joon@joons-MacBook-Pro.local>
2018-12-14 23:32:53 -0500

    client loop updated

--- client_loop_hello.py
+++ client_loop_hello.py
@@ -1,6 +1,6 @@
 import os
 for value in range(1,100):
-	#os.system('python hello_client.py')
-	#os.system('python subtracter_client.py')
+	os.system('python hello_client.py')
+	os.system('python subtracter_client.py')
 	os.system('python adder_client.py')
 print 'All done'
